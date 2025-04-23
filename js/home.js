9872d813c003b0d78437b38b57da2ff5527ffb20
excel <axceldesigns@gmail.com>
2025-04-23 20:13:15 +0100

    intern

---
+++ js/home.js
@@ -0,0 +1,6 @@
+const ham = document.getElementById('ham')
+ham.addEventListener('click',()=>{
+    document.querySelector('nav ul').style.display = 'flex'
+    document.querySelector('nav ul').style.marginTop = '3vh'
+    document.querySelector('nav ul').style.flexDirection = 'column'
+})
