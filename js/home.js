2ba6a79ad4d9501790da0801358d7fff468ca7d4
excel <axceldesigns@gmail.com>
2025-04-23 20:23:39 +0100

    intern

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 const ham = document.getElementById('ham')
 ham.addEventListener('click',()=>{
+    // document.querySelector('nav ul').style.display === 'flex' ? document.querySelector('nav ul').style.display = 'none' : document.querySelector('nav ul').style.display = 'flex' 
     document.querySelector('nav ul').style.display = 'flex'
     document.querySelector('nav ul').style.marginTop = '3vh'
     document.querySelector('nav ul').style.flexDirection = 'column'
